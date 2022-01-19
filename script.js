8cbb4c9648eac0dec43f7022d2c5f05507cdee40
Harmeet Matharoo <hmjatt@gmail.com>
2022-01-18 20:08:06 -0700

    fixed color Picker input ele

--- script.js
+++ script.js
@@ -3,14 +3,17 @@
 const container = document.getElementById("grid-container");
 const mode = document.getElementById("mode");
 const colors = document.getElementById("colors");
-const colorOption = document.getElementById("color-option");
 const colorPicker = document.getElementById("color-picker");
 const resetButton = document.querySelector("button");
+const sizeInput = document.getElementById("size");
+
+let getColor;
+let colorModeColor;
+let colorModeChanged;
+let rgbValue;
+let colorSelected;
 
 
-let red = {};
-let green = {};
-let blue = {};
 
 
 resetButton.addEventListener("click", function reset() {
@@ -47,75 +50,48 @@ function makeGrid(rows, columns) {
         container.appendChild(cell).className = "grid-cell";
         cell.addEventListener('mouseover', function color(){
             
-            
             //generate opacity, based on cell's previous opacity
-            function opacity() {
+            window.opacity = function() {
+
                 let opacity = window.getComputedStyle(cell).backgroundColor;
+
                 const myArray = opacity.split(", ");
                 let previousOpacity = myArray.slice(-1)[0];
                 let previousOpacityInt = parseFloat(previousOpacity);
                 let newOpacity =  previousOpacityInt + modeChange(); //grab opacity from modeChange fxn
+
                 return newOpacity;
             }
-            
 
-            //change the color depending on the Color selected
-            window.colorChange = function() {
+  
 
 
-                let color;
-                //generate random color
-                        
-                const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-                // create color with generated opacity
+            //create color based on color Selected
+            if(colorSelected === true) {
+
                 
 
+                colorPick();
 
-                if(colors.value == "rainbow") {
-                    //range is between all colors
-                    const randomByte = () => randomNumber(0, 255);
-                    color = `rgba(${[randomByte(), randomByte(), randomByte(), opacity()].join(',')})`;
-                } else if(colors.value == "greyscale") {
-                    //range between greys(all rbg values same eg. rbg(16, 16, 16))
-                    const randomByte = () => randomNumber(0, 125);
-                    let colorGenerated = randomByte();
-                    // console.log(randomByte());
-                    color = `rgba(${[colorGenerated, colorGenerated, colorGenerated, opacity()].join(',')})`;
-                } else {
-                    
-                    color = `rgba(${[red.r, green.g, blue.b, opacity()].join(',')})`;
-                }
-                // else if(colors.value == "color") {
+                getColor = rgbValue;
 
-                //     //show the player a rbg chart and grab value of color from there
-                //     colorOption = '<input type="color" id="color" name="color" value="red">';
-
-
-                //     const randomByte = () => randomNumber(0, 255);
-                //     randomColor = `rgba(${[randomByte(), randomByte(), randomByte(), opacity()].join(',')})`;
-                // }
-
-                // console.log(randomColor);
-
-
-
-
-
-
-                console.log(color);
-                return(color);
-
-            }
+            } else if(colorModeChanged === true) {
             
-    
-            cell.style.backgroundColor = colorChange();
+
+                colorChange();
+
+                getColor = colorModeColor;
+            } else {
+                getColor = "rgb(0, 0, 0)"
+            }           
+
+            //onload color is black
+            
+
+            
+            cell.style.backgroundColor = getColor;
         });
-
-    
-
-
-        // cell.style.backgroundColor = colorPick();
         
     }
    
@@ -126,6 +102,8 @@ makeGrid(16, 16);
 
 //increament the opacity depending on the Mode selected
 function  modeChange() {
+
+    colorSelected = false;
 
     let opChange = 0.00;
 
@@ -141,23 +119,69 @@ function  modeChange() {
 }
 
 
-function colorPick() {
-    let h = colorPicker.value;
+//change the color
+function colorChange() {
 
-    let r = 0, g = 0, b = 0;
-
-    // 3 digits
-    if (h.length == 4) {
-      r = "0x" + h[1] + h[1];
-      g = "0x" + h[2] + h[2];
-      b = "0x" + h[3] + h[3];
-  
-    // 6 digits
-    } else if (h.length == 7) {
-      red.r = "0x" + h[1] + h[2];
-      green.g = "0x" + h[3] + h[4];
-      blue.b = "0x" + h[5] + h[6];
-    }
+    colorSelected = false;
+    colorModeChanged = true;
+    
 
     
+    //generate random color
+            
+    const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+    
+
+    
+    // create color with generated opacity
+    if(colors.value == "rainbow") {
+
+
+        //range is between all colors
+        const randomByte = () => randomNumber(0, 255);
+
+        colorModeColor = `rgba(${[randomByte(), randomByte(), randomByte(), opacity()].join(',')})`;
+    } else if(colors.value == "greyscale") {
+
+
+        //range between greys(all rbg values same eg. rbg(16, 16, 16))
+        const randomByte = () => randomNumber(0, 125);
+
+        let colorGenerated = randomByte();
+
+        colorModeColor = `rgba(${[colorGenerated, colorGenerated, colorGenerated, opacity()].join(',')})`;
+    } 
+
+    return colorModeColor;
+
+}
+
+
+
+    
+//convert hex from input element to RGB without opacity
+function colorPick() {
+
+    let hex = colorPicker.value;
+
+    colorSelected = true;
+    colorModeChanged = false;
+
+    let r = parseInt(hex.slice(1, 3), 16);
+    let g = parseInt(hex.slice(3, 5), 16);
+    let b = parseInt(hex.slice(5, 7), 16);
+
+    rgbValue = "rgb(" + r + "," + g + "," + b + ")";
+
+    
+    return rgbValue;
+
+}
+
+
+//change the size of the pixels/cells
+
+function changeSize() {
+    console.log(sizeInput.value);
 }
